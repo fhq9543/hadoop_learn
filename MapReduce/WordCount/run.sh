7bcdfac470666f744ba509695fdf6f06cbc7f765
sandy.fang <751541230@qq.com>
2018-03-16 11:17:48 +0800

    update

--- MapReduce/WordCount/run.sh
+++ MapReduce/WordCount/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-hadoop jar $STREAM  \
--mapper ./mapper.py \
--reducer ./reducer.py \
--input /input/*.txt \
--output /output
+HADOOP_CMD="/usr/local/hadoop/bin/hadoop"
+STREAM_JAR_PATH="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-*.jar"
+INPUT_FILE_PATH="/input"
+OUTPUT_PATH="/output"
+
+$HADOOP_CMD fs -rm -r $OUTPUT_PATH
+$HADOOP_CMD jar $STREAM_JAR_PATH \
+    -input $INPUT_FILE_PATH \
+    -output $OUTPUT_PATH \
+    -mapper "python mapper.py" \
+    -reducer "python reducer.py" \
+    -file ./mapper.py \
+    -file ./reducer.py
